0a6d44c36b64a03348263fe1dc9383fc24684acb
Rastin Badpeyma <rastinbadpeyma@gmail.com>
2025-05-10 23:51:40 +0330

    business logic rules

--- src/core/domain/entities/user.entity.ts
+++ src/core/domain/entities/user.entity.ts
@@ -3,5 +3,21 @@ export class User {
     public readonly id: number,
     public readonly email: string,
     public readonly password: any,
-  ) {}
+  ) {
+    this.validateEmail(email);
+    this.validatePassword(password);
+  }
+
+  // Business Rules
+  private validatePassword(password: any) {
+    if (password.length < 8) {
+      throw new Error('Password must be at least 8 characters long');
+    }
+  }
+
+  private validateEmail(email: string) {
+    if (!email.includes('@') || !email.includes('.')) {
+      throw new Error('Invalid email address');
+    }
+  }
 }
